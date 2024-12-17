6acf052f7df73dc0cd8e344363501b986f64452a
ts_dev <writer_fairy_road@yahoo.co.jp>
2024-12-17 21:26:23 +0900

    Add Customer class

---
+++ src/Persons/Customers/Customer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Persons\Customers;
+
+use Persons\Person;
+
+class Customer extends Person {
+  private array $interestedCategories;
+
+  public function __construct($name, $age, $address, $interestedCategories)
+  {
+    parent::__construct($name, $age, $address);
+    $this->interestedCategories = $interestedCategories;
+  }
+
+  
+  public function interestedCategories(Restaurant $restautant): array{
+    // Restaurant を引数に取り、そのレストランが持っている自分が興味のある食品カテゴリのリストを返します。
+    return [];
+  }
+
+  // TODO: Remove null in Return Type
+  public function order(Restaurant $restaurant, array $categories): ?Invoice {
+    return null;
+  }
+}
